61d30a929be446f1143fecd4af493543e5c0554d
Bohdan Tkhoryk <thorykbv@gmail.com>
2024-02-07 22:33:59 +0200

    UserRoom

--- src/components/pages/UserRoom.jsx
+++ src/components/pages/UserRoom.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const UserRoom = ({ room }) => {
+const UserRoom = () => {
+  const [date, setDate] = useState(null);
+  const [taskTitle, setTaskTitle] = useState("");
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleAddRoom = () => {};
+  const handleInputChange = (e) => {
+    setTaskTitle(e.target.value);
+  };
   return (
-    <div className="container text-center">
-      <h1 className="text-center">User Room</h1>
-      
-    </div>
+    <Container className="text-center">
+      <Row className="justify-content-center">
+        <Col md={4}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              id="taskTitle"
+              name="taskTitle"
+              placeholder="Enter task title"
+              value={taskTitle}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Col>
+        <Col md={4}>
+          <Form.Group>
+            <Form.Control
+              type="date"
+              id="taskDate"
+              name="taskDate"
+              value={date}
+              onChange={handleDateChange}
+            />
+          </Form.Group>
+        </Col>
+        <Col md={4}>
+          <Button onClick={handleAddRoom} className="w-100">
+            Add task
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
